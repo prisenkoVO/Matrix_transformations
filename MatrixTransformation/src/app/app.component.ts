270faa5c309dad05122440833d5d63745e5e7f17
prisenkovo <rsomotors@gmail.com>
2019-11-05 20:53:57 +0300

    add func to change size of matrix

--- MatrixTransformation/src/app/app.component.ts
+++ MatrixTransformation/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MarixTransformation';
+  
+  @Input() columns: number;
+  @Input() rows: number;
+
+  @Output() submitColumns: EventEmitter<Array<number>> = new EventEmitter();
+  @Output() submitRows: EventEmitter<Array<number>> = new EventEmitter();
+
+  listColumns:Array<number> = [];
+  listRows:Array<number> = [];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public onSubmit(columns:number, rows:number){
+    
+    // const countColumns:Array<number> = [];
+    // const countRows:Array<number> = [];
+    this.listColumns = [];
+    this.listRows = [];
+    for (var i = 1; i <= columns; i++) {
+      this.listColumns.push(i);
+    }
+    
+    for (var i = 1; i <= rows; i++) {
+      this.listRows.push(i);
+    }
+
+    this.submitColumns.emit(this.listColumns)
+    this.submitRows.emit(this.listRows)
+    
+  }
 }
--- MatrixTransformation/src/app/components/input-matrix/input-matrix.component.ts
+++ MatrixTransformation/src/app/components/input-matrix/input-matrix.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-matrix',
@@ -7,12 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputMatrixComponent implements OnInit {
 
-  countColumns = [1,2,3]
-  countRows = [1,2,3]
+  @Input() listColumns:Array<number>
+  @Input() listRows:Array<number>
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngDoCheck(){
+    console.log(this.listColumns)
+    console.log(this.listRows)
   }
 
 }
