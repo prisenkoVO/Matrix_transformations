b03f5cb0d348d41ab00f33ab2dc2b79f846eda1a
prisenkovo <rsomotors@gmail.com>
2019-11-06 16:07:53 +0300

    comm

--- MatrixTransformation/src/app/components/input-matrix/input-matrix.component.ts
+++ MatrixTransformation/src/app/components/input-matrix/input-matrix.component.ts
@@ -1,23 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, ViewContainerRef, ViewRef } from '@angular/core';
 
 @Component({
   selector: 'app-input-matrix',
   templateUrl: './input-matrix.component.html',
   styleUrls: ['./input-matrix.component.scss']
 })
+
 export class InputMatrixComponent implements OnInit {
+
+  
 
   @Input() listColumns:Array<number>
   @Input() listRows:Array<number>
+
+  @Input() column: number
+  @Input() row: number
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  ngDoCheck(){
-    console.log(this.listColumns)
-    console.log(this.listRows)
+
+  multiplyOnUnitMatrix(UserMatrix){}
+
+
+  multiplyMatrix(A,B)
+  {
+    var rowsA = A.length, colsA = A[0].length,
+        rowsB = B.length, colsB = B[0].length,
+        C = [];
+    if (colsA != rowsB) return false;
+    for (var i = 0; i < rowsA; i++) C[ i ] = [];
+    for (var k = 0; k < colsB; k++)
+     { for (var i = 0; i < rowsA; i++)
+        { var t = 0;
+          for (var j = 0; j < rowsB; j++) t += A[ i ][j]*B[j][k];
+          C[ i ][k] = t;
+        }
+     }
+    return C;
   }
 
 }
